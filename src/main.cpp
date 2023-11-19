47ffcc33f9058f728fb3cbfee6f115c51494aa11
Brady Bangasser <bradybbangasser@gmail.com>
2023-11-19 11:24:07 -0600

    multifile

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "mylib.hpp"
+#include "test.h"
 
 int main() {
     MyLib lib;
-    // lib.setName("this");
+    lib.setName("this");
+
+    printTest();
+    
     std::cout << lib.getName() << std::endl;
     return 0;
 }
